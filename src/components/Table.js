b947af51b54a0f1c3193ff4ee57c11e9850d5435
paspheeris <pa.spheeris@gmail.com>
2017-11-10 14:34:47 -0800

    Add icons to indicate the sort ordering.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TableRow from './TableRow';
 import { SORT_TYPES } from '../actions/actionTypes';
+/* import {'caret-down.svg'} from '../.././public';*/
 
 class Table extends React.Component {
   constructor() {
@@ -27,7 +28,35 @@ class Table extends React.Component {
       this.props.sort(SORT_TYPES.ALL_TIME_ASC) :
       this.props.sort(SORT_TYPES.ALL_TIME_DESC)
   }
+    chooseImage = (section, currentSort) => {
+        /* const currentSort = this.props.sortBy;*/
+        const down = "caret-down.svg";
+        const up = "caret-up.svg";
+        const right = "caret-right.svg";
+                console.log('currentSort:', currentSort);
+                console.log('section:', section);
+        switch(section) {
+            case 'name':
+                if(currentSort === SORT_TYPES.NAME_DESC) return down;
+                else if (currentSort === SORT_TYPES.NAME_ASC) return up;
+                else return right;
+                break;
+            case 'past30':
+                if(currentSort === SORT_TYPES.PAST_30_DESC) return down;
+                else if (currentSort === SORT_TYPES.PAST_30_ASC) return up;
+                else return right;
+                break;
+            case 'allTime':
+                if(currentSort === SORT_TYPES.ALL_TIME_DESC) return down;
+                else if (currentSort === SORT_TYPES.ALL_TIME_ASC) return up;
+                else return right;
+                break;
+            default: return right;
+        }
+        /* return right;*/
+    }
   render() {
+      let {sortBy} = this.props;
     return (
       <table className="table">
         <thead>
@@ -36,11 +65,23 @@ class Table extends React.Component {
             <td>#</td>
             <td className='profile-pic-column'></td>
             <td onClick={this.cb}>
-              Camper Name</td>
+                Camper Name
+                <img className="caret"
+                     src={this.chooseImage('name', sortBy)}
+                     alt="caret" />
+            </td>
             <td onClick={this.cb}>
-              Points in past 30 days</td>
+                Points in past 30 days
+                <img className="caret"
+                     src={this.chooseImage('past30', sortBy)}
+                     alt="caret" />
+            </td>
             <td onClick={this.cb}>
-              All time points</td>
+                All time points
+                <img className="caret"
+                     src={this.chooseImage('allTime', sortBy)}
+                     alt="caret" />
+            </td>
           </tr>
         </thead>
         <tbody>
